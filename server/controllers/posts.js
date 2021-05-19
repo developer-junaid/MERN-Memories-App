a827e9589a433af954797721f7268e61f1469a08
developer-junaid <qjunaid623@gmail.com>
2021-05-19 09:16:13 +0500

    Fixed Typo in controller, postdelete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -60,7 +60,7 @@ export const deletePost = async (req, res) => {
   const { id } = req.params;
 
   // Check
-  if (!mongoose.Types.ObjectId.isValid(_id)) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send("No post with that id");
   }
 
