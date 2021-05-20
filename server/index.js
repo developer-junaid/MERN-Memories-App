5ffb8aaa1ea967c3433926fe596a5ddf4a33407c
developer-junaid <qjunaid623@gmail.com>
2021-05-20 22:05:33 +0500

    Deploy Backend to Heroku

--- server/index.js
+++ server/index.js
@@ -20,17 +20,17 @@ app.use(cors());
 app.use("/posts", postRoutes); // Each route start with /posts/route
 
 app.get("/", (req, res) => {
-  res.send("Hello to Memories API");
+  res.send("Hellow to Memories API");
 });
-
+// Update
 // Connect to DB
-const username = "admin";
-const password = "admin";
-const CONNECTION_URL = `mongodb+srv://${username}:${password}@mongocluster.xesxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
