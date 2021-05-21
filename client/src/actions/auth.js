9ce0ef629588709d3d97a7fb719f04e51ae9a007
developer-junaid <qjunaid623@gmail.com>
2021-05-21 13:26:38 +0500

    Resolved bug in Client 'actions/auth'

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,24 +1,22 @@
 import * as api from "../api";
 import { AUTH } from "./../constants/actionTypes";
 
+export const signin = (formData, history) => async (dispatch) => {
+  try {
+    // login the user ...
 
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const signin = (formData, history) = async(dispatch) =>{
-    try {
-        // login the user ...
+export const signup = (formData, history) => async (dispatch) => {
+  try {
+    // signup the user ...
 
-        history.push('/')
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export const signup = (formData, history) = async(dispatch) =>{
-    try {
-        // signup the user ...
-
-        history.push('/')
-    } catch (error) {
-        console.log(error);
-    }
-}
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
