2cb35955b3e6f4999f9c93b96c7a5241e063fdff
developer-junaid <qjunaid623@gmail.com>
2021-05-21 08:54:04 +0500

    Made Input as a Separate Component, Added Form Template

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
 
- const Auth = () => {
-    const state = null 
-    return (
-        <div>
-            AUTH
-        </div>
-    )
-}
+// Material-UI
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
 
-export default Auth
+// Icons
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+// Styles
+import useStyles from "./styles";
+import Input from "./Input";
+
+const Auth = () => {
+  const classes = useStyles();
+
+  const isSignup = false;
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Handlers
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Return
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={3}>
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignup ? "Sign up" : "Sign In"}
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+// Material-UI
+import { TextField, Grid, IconButton, InputAdornment } from "@material-ui/core";
+
+// Icons
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
+const Input = ({
+  half,
+  name,
+  label,
+  handleChange,
+  autoFocus,
+  type,
+  handleShowPassword,
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        label={label}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password" ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
+        }
+      />
+    </Grid>
+  );
+};
+
+export default Input;
