1e8e1503199e807b3eea19dee6c573a5113e7a4a
developer-junaid <qjunaid623@gmail.com>
2021-05-21 07:58:32 +0500

    Added React router dom

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// React Router DOM
+import { Link } from "react-router-dom";
+
 // Material-ui
 import { AppBar, Typography } from "@material-ui/core";
 
@@ -14,15 +17,23 @@ const Navbar = () => {
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Typography className={classes.heading} variant="h2" align="center">
-        Memories
-      </Typography>
-      <img
-        className={classes.image}
-        src={memories}
-        alt="memories"
-        height="60"
-      />
+      <div className={classes.brandContainer}>
+        <Typography
+          className={classes.heading}
+          component={Link}
+          to="/"
+          variant="h2"
+          align="center"
+        >
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </div>
     </AppBar>
   );
 };
