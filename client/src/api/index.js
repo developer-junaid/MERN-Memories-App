b6a1845089a87dd215fb663d1df441f857d5ce4a
developer-junaid <qjunaid623@gmail.com>
2021-05-24 10:09:20 +0500

    Deployed Backend and Frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "https://memories-manager.herokuapp.com" });
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://memories-manager.herokuapp.com" });
 
 // Add Interceptor
 API.interceptors.request.use((req) => {
