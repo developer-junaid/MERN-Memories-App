83ec4e363727f345a875a496eef30432fc2576ac
Developer Junaid <qjunaid623@gmail.com>
2023-07-22 15:12:44 +0500

    changed a few stuff

---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+
+import dotenv from "dotenv"
+
+import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/users.js"
+
+// Initialize app
+const app = express()
+dotenv.config()
+
+// For sending requests
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+// Routes and Middleware
+app.use("/posts", postRoutes) // Each route start with /posts/route
+app.use("/users", userRoutes)
+
+app.get("/", (req, res) => {
+  res.send("Hellow to Memories API")
+})
+// Connect to DB
+const PORT = process.env.PORT || 5001
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message))
+
+mongoose.set("useFindAndModify", false)
